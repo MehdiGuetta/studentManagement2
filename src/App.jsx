19f47ac1d7ffa3729e4b374283b943817aa3caf9
MehdiGuetta <moustafaxda@gmail.com>
2024-12-21 03:05:43 +0100

    editing some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./Pages/Auth";
 import RegistrationForm from "./Pages/Register";
-import Dashboard from "./Pages/Dashboard";
+import AdminDashboard from "./Pages/AdminDashboard";
 import NotFound from "./NotFound";
 import HomePage from "./Layout/NavItems/Home";
 import UserProfile from "./Layout/NavItems/UserProfile";
@@ -9,6 +9,7 @@ import EditColor from "./Layout/NavItems/EditColor";
 import UserList from "./Layout/NavItems/UserList";
 import AddUser from "./Layout/NavItems/AddUser";
 import Requests from "./Layout/NavItems/Requests";
+import UserDashboard from "./Pages/UserDashboard";
 
 const App = () => {
   return (
@@ -16,9 +17,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Auth />} />
         <Route path="/register" element={<RegistrationForm />} />
-        <Route path="/dashboard" element={<Dashboard />}>
-          <Route index element={<HomePage />} />
-          <Route path="home" element={<HomePage />} />
+        <Route path="/user-dashboard" element={<UserDashboard />} />
+        <Route path="/admin-dashboard" element={<AdminDashboard />}>
+          <Route index path="home" element={<HomePage />} />
           <Route path="profile" element={<UserProfile />} />
           <Route path="edit-color" element={<EditColor />} />
           <Route path="users-list" element={<UserList />} />
