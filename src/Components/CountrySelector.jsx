dd74a18fbfce3126b1c7efeff4ba7b8b1042291a
MehdiGuetta <moustafaxda@gmail.com>
2024-12-27 23:44:06 +0100

    components responsivity

--- src/Components/CountrySelector.jsx
+++ src/Components/CountrySelector.jsx
@@ -12,7 +12,7 @@ const CountrySelector = ({ name, onChange }) => {
       <select
         name={name}
         onChange={onChange}
-        className="w-96 py-3 px-4 border-none outline-blue-500 bg-[#eee] text-black rounded-md"
+        className="w-full py-3 px-4 border-none outline-blue-500 bg-[#eee] text-black rounded-md"
       >
         <option value="">Select Country</option>
         {countries.map((country, index) => (
--- src/Components/PasswordInput.jsx
+++ src/Components/PasswordInput.jsx
@@ -5,7 +5,7 @@ function PasswordInput({ name, placeholder, value, onChange, required }) {
   const [showPassword, setShowPassword] = useState(false);
 
   return (
-    <div className="relative w-96">
+    <div className="relative w-full max-w-md">
       <input
         className="w-full py-3 px-4 border-none outline-blue-500 bg-[#eee] text-black rounded-md"
         type={showPassword ? "text" : "password"}
@@ -17,7 +17,7 @@ function PasswordInput({ name, placeholder, value, onChange, required }) {
       />
       <span
         onClick={() => setShowPassword((prev) => !prev)}
-        className="absolute right-4 top-3.5 cursor-pointer text-gray-600"
+        className="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-600"
       >
         {showPassword ? <FaEyeSlash size={20} /> : <FaEye size={20} />}
       </span>
