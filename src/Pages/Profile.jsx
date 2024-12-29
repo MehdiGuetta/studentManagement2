4671b80f58072058d852d555d15e3d87a932d247
MehdiGuetta <moustafaxda@gmail.com>
2024-12-29 21:28:58 +0100

    changed nothing

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
   const user = useSelector((state) => state.user);
-  const color = user.couleur // color in API
+  const color = user.couleur;
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
