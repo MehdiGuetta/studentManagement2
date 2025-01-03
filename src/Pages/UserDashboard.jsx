38df1c55cafd1556fd669800f0d1308a3b981b08
MehdiGuetta <moustafaxda@gmail.com>
2025-01-03 15:57:41 +0100

    styling login back i user dashboard

--- src/Pages/UserDashboard.jsx
+++ src/Pages/UserDashboard.jsx
@@ -12,9 +12,16 @@ const UserDashboard = () => {
 
   if (!user) {
     return (
-      <div className="login-back">
-        <p>No user data available. Please log in.</p>
-        <button onClick={() => navigate("../")}>Log in Page</button>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <p className="text-lg text-center mb-4">
+          Unfortunately.. No user data available. Please log in.
+        </p>
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-lg px-16 py-3 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          onClick={() => navigate("../")}
+        >
+          Log in
+        </button>
       </div>
     );
   }
@@ -23,8 +30,8 @@ const UserDashboard = () => {
     <div className="flex flex-col min-h-screen">
       <Header />
       <UserNavigationBar />
-      <main className="flex-1">
-        <div className="flex justify-between">
+      <main className="flex-1 flex">
+        <div className="flex w-full">
           <UserSideBar />
           <Section />
         </div>
